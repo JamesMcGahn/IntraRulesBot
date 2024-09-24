08eb7604159d87ea522c3a9f0769f0ff1c2f29c2
James McGahn <jmcgahn@gmail.com>
2024-09-23 23:34:40 -0400

    rf main add rule logic into rule_worker

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ from actions_worker import ActionsWorker
 from conditions_worker import ConditionsWorker
 from keys import keys
 from login_manager import LoginManager
+from rule_worker import RuleWorker
 from services.logger import Logger
 from trigger_worker import TriggerWorker
 from web_element_interactions import WaitConditions, WebElementInteractions
@@ -67,156 +68,14 @@ class WebScrape:
 
                 sleep(3)
                 for rule in self.config_data["rules"]:
-                    addRuleBtn = self.wELI.wait_for_element(
-                    10,
-                    By.ID,
-                    "ctl00_ActionBarContent_rbAction_Add",
-                    WaitConditions.CLICKABLE,
-                )
-                    if addRuleBtn:
-                        self.rule_condition_queues_source = "queues"
-                        addRuleBtn.click()
-                        # --> Switch to Module Add Rule Popup
-
-                        WebDriverWait(self.driver, 10).until(
-                            EC.frame_to_be_available_and_switch_to_it(
-                                (By.NAME, "RadWindowAddEditRule")
-                            )
-                        )
-                        # --> If Tuturial Page Info Present -> Skip Page
-                        if self.wELI.wait_for_element(
-                            15,
-                            By.XPATH,
-                            '//*[contains(@id, "overlayButtonsLeft_cbDontAskLead")]',
-                            WaitConditions.CLICKABLE,
-                        ):
-                            continue_btn = self.wELI.wait_for_element(
-                                15,
-                                By.XPATH,
-                                '//*[contains(@id, "overlayButtons_rbContinue_input")]',
-                                WaitConditions.CLICKABLE,
-                            )
-                            sleep(3)
-                            continue_btn.click()
-
-                            # Set Rule Name
-                        rule_name_input = self.wELI.wait_for_element(
-                            15,
-                            By.XPATH,
-                            '//*[contains(@id, "overlayRuleProgressArea_tbRuleName")]',
-                            WaitConditions.VISIBILITY,
-                        )
-                        rule_name_input.send_keys(rule["rule_name"])
-
-                        #Trigger
-                        trigger = TriggerWorker(self.driver, rule)
-                        trigger.do_work()
-
-                        continue_btn.click()
-                        # ------>>> Continue to Condition Page
-                        
-                        
-                        conditions = ConditionsWorker(self.driver, rule)
-                        if "conditions" in rule and rule["conditions"]:
-                            conditions.do_work()
-                            continue_btn.click()
-                        # --->> action page -selection
-                        sleep(3)
-                        actions = ActionsWorker(self.driver,rule)
-                        if "actions" in rule and rule["actions"]:
-                            actions.rule_condition_queues_source = conditions.rule_condition_queues_source
-                            actions.do_work()
-
-                        # Rule category
-                        rule_settings_hamburger = self.wELI.wait_for_element(
-                            15,
-                            By.XPATH,
-                            '//*[contains(@id, "overlayContent_divAddEditAction")]/div[3]/a',
-                            WaitConditions.CLICKABLE,
-                        )
-                        rule_settings_hamburger.click()
-                        sleep(3)
-
-                        self.driver.switch_to.default_content()
-
-                        WebDriverWait(self.driver, 10).until(
-                            EC.frame_to_be_available_and_switch_to_it(
-                                (By.NAME, "RadWindowAddEditRuleSettings")
-                            )
-                        )
-
-                        rule_category_dropdown_arrow = self.wELI.wait_for_element(
-                            15,
-                            By.XPATH,
-                            '//*[contains(@id, "ddRuleCategory_Arrow")]',
-                            WaitConditions.CLICKABLE,
-                        )
-                        rule_category_dropdown_arrow.click()
-
-                        self.wELI.select_item_from_list(
-                            10,
-                            By.XPATH,
-                            '//*[contains(@id, "ddRuleCategory_DropDown")]/div/ul/li',
-                            "Other - Admin",
-                        )
-                        sleep(3)
-                        self.driver.switch_to.default_content()
-                        WebDriverWait(self.driver, 10).until(
-                            EC.frame_to_be_available_and_switch_to_it(
-                                (By.NAME, "RadWindowAddEditRule")
-                            )
-                        )
-
-                        continue_btn.click()
-                        sleep(3)
-                        submit_btn = self.wELI.wait_for_element(
-                            15,
-                            By.XPATH,
-                            '//*[contains(@id, "overlayButtons_rbSubmit_input")]',
-                            WaitConditions.CLICKABLE,
-                        )
-                        submit_btn.click()
-
-                        try:
-                            WebDriverWait(self.driver, 3).until(EC.alert_is_present())
-
-                            alert = self.driver.switch_to.alert
-                            if alert.text == f"A Rule with the name {rule["rule_name"]} already exists.":
-                                alert.accept()
-                                # TODO Ask user to update
-                                
-                                Logger().insert(
-                                "Rule ",
-                                "INFO",
-                            )   
-                                raise ValueError(alert.text)
-                            else:
-                                raise ValueError(alert.text)
-                                Logger().insert(
-                                "Session open elsewhere alert was present. Accepted alert to proceed with this session.",
-                                "INFO",
-                            )
-                        except TimeoutException:
-                            Logger().insert(f"Rule: {rule["rule_name"]} has been submitted.","INFO")
-                        
-                        self.driver.switch_to.default_content()
-                        success_message = self.wELI.wait_for_element(
-                                            15,
-                                            By.XPATH,
-                                            '//*[contains(@id, "lblMessage")]',
-                                            WaitConditions.VISIBILITY,
-                                        )
-                        if "You have successfully added the Rule" in success_message.text:
-                            Logger().insert(f"Rule: {rule["rule_name"]} has been created.","INFO")
-            sleep(30)
+                    ruleworker = RuleWorker(self.driver, rule)
+                    ruleworker.do_work()
+                sleep(30)
 
         except Exception as e:
             print(e)
         finally:
             self.close()
-
-
-
 
 
 run = WebScrape(keys["url"])
---
+++ rule_worker.py
@@ -0,0 +1,177 @@
+import json
+from time import sleep
+
+from selenium import webdriver
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    NoSuchFrameException,
+    NoSuchWindowException,
+    StaleElementReferenceException,
+    TimeoutException,
+)
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+from actions_worker import ActionsWorker
+from conditions_worker import ConditionsWorker
+from keys import keys
+from login_manager import LoginManager
+from services.logger import Logger
+from trigger_worker import TriggerWorker
+from web_element_interactions import WaitConditions, WebElementInteractions
+
+
+class RuleWorker:
+
+    def __init__(self, driver, rule):
+        self.driver = driver
+        self.rule = rule
+        self.wELI = WebElementInteractions(self.driver)
+
+    def do_work(self):
+        addRuleBtn = self.wELI.wait_for_element(
+            10,
+            By.ID,
+            "ctl00_ActionBarContent_rbAction_Add",
+            WaitConditions.CLICKABLE,
+        )
+        if addRuleBtn:
+            addRuleBtn.click()
+            self.switch_to_rule_module()
+
+            if self.is_tutorial_page_present():
+                self.next_page()
+
+            self.set_rule_name()
+            self.start_trigger_page()
+            self.next_page()
+            self.start_conditions_page()
+            self.next_page()
+            self.start_actions_page()
+            self.set_rule_category()
+            self.next_page()
+            self.submit_rule()
+
+    def start_trigger_page(self):
+        trigger = TriggerWorker(self.driver, self.rule)
+        trigger.do_work()
+
+    def start_conditions_page(self):
+        self.conditions = ConditionsWorker(self.driver, self.rule)
+        if "conditions" in self.rule and self.rule["conditions"]:
+            self.conditions.do_work()
+
+    def start_actions_page(self):
+        self.actions = ActionsWorker(self.driver, self.rule)
+        if "actions" in self.rule and self.rule["actions"]:
+            self.actions.rule_condition_queues_source = (
+                self.conditions.rule_condition_queues_source
+            )
+            self.actions.do_work()
+
+    def switch_to_rule_module(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.frame_to_be_available_and_switch_to_it((By.NAME, "RadWindowAddEditRule"))
+        )
+
+    def set_rule_name(self):
+        rule_name_input = self.wELI.wait_for_element(
+            15,
+            By.XPATH,
+            '//*[contains(@id, "overlayRuleProgressArea_tbRuleName")]',
+            WaitConditions.VISIBILITY,
+        )
+        rule_name_input.send_keys(self.rule["rule_name"])
+
+    def next_page(self):
+        continue_btn = self.wELI.wait_for_element(
+            15,
+            By.XPATH,
+            '//*[contains(@id, "overlayButtons_rbContinue_input")]',
+            WaitConditions.CLICKABLE,
+        )
+        sleep(3)
+        continue_btn.click()
+
+    def is_tutorial_page_present(self):
+        return self.wELI.wait_for_element(
+            15,
+            By.XPATH,
+            '//*[contains(@id, "overlayButtonsLeft_cbDontAskLead")]',
+            WaitConditions.CLICKABLE,
+        )
+
+    def submit_rule(self):
+        submit_btn = self.wELI.wait_for_element(
+            15,
+            By.XPATH,
+            '//*[contains(@id, "overlayButtons_rbSubmit_input")]',
+            WaitConditions.CLICKABLE,
+        )
+        submit_btn.click()
+        self.wait_for_dup_rule_alert()
+        self.driver.switch_to.default_content()
+        success_message = self.wELI.wait_for_element(
+                                            15,
+                                            By.XPATH,
+                                            '//*[contains(@id, "lblMessage")]',
+                                            WaitConditions.VISIBILITY,
+                                        )
+        if "You have successfully added the Rule" in success_message.text:
+            Logger().insert(f"Rule: {self.rule["rule_name"]} has been created.","INFO")
+        else:
+            Logger().insert(f"Recevied no success feedback. Cannot confirm if Rule: {self.rule["rule_name"]} has been created.","WARN")
+
+
+
+    def set_rule_category(self):
+        rule_settings_hamburger = self.wELI.wait_for_element(
+            15,
+            By.XPATH,
+            '//*[contains(@id, "overlayContent_divAddEditAction")]/div[3]/a',
+            WaitConditions.CLICKABLE,
+        )
+        rule_settings_hamburger.click()
+        sleep(3)
+        self.driver.switch_to.default_content()
+        self.wELI.switch_to_frame(10, By.NAME, "RadWindowAddEditRuleSettings")
+        rule_category_dropdown_arrow = self.wELI.wait_for_element(
+            15,
+            By.XPATH,
+            '//*[contains(@id, "ddRuleCategory_Arrow")]',
+            WaitConditions.CLICKABLE,
+        )
+        rule_category_dropdown_arrow.click()
+
+        self.wELI.select_item_from_list(
+            10,
+            By.XPATH,
+            '//*[contains(@id, "ddRuleCategory_DropDown")]/div/ul/li',
+            "Other - Admin",
+        )
+        sleep(3)
+        self.driver.switch_to.default_content()
+        self.wELI.switch_to_frame(10, By.NAME, "RadWindowAddEditRule")
+
+    def wait_for_dup_rule_alert(self):
+        try:
+            WebDriverWait(self.driver, 3).until(EC.alert_is_present())
+
+            alert = self.driver.switch_to.alert
+            if alert.text == "A Rule with this name already exists":
+                alert.accept()
+                                # TODO Ask user to update
+                                
+                Logger().insert(
+                                f"A Rule with the name {self.rule["rule_name"]} already exists.",
+                                "ERROR",
+                            )   
+                raise ValueError(alert.text)
+                            
+        except TimeoutException:
+            Logger().insert(f"Rule: {self.rule["rule_name"]} has been submitted.","INFO")
