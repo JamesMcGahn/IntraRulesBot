c56d13ee59594baf8a3f0ce36fd016eb7e42e036
James McGahn <jmcgahn@gmail.com>
2024-10-01 12:37:44 -0400

    create widgetfactory class

---
+++ components/helpers/widget_factory/widget_factory.py
@@ -0,0 +1,58 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QFormLayout, QGroupBox, QLabel, QLineEdit, QSizePolicy
+
+from components.boxes import GradientGroupBox
+
+
+class WidgetFactory:
+
+    @staticmethod
+    def create_form_box(
+        title,
+        parent_layout,
+        gradient_box=False,
+        border_color=None,
+        drop_shadow_effect="default",
+        object_name="",
+    ):
+        if gradient_box:
+            box = GradientGroupBox(
+                title, "black", gradient_box, border_color, drop_shadow_effect
+            )
+            box.set_gradient_start_stop(
+                box.width() / 2, 0, box.width() / 2, box.height()
+            )
+        else:
+            box = QGroupBox(title)
+            box.setObjectName(object_name)
+
+        box.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+
+        layout = QFormLayout(box)
+        layout.setVerticalSpacing(25)
+        layout.setFormAlignment(Qt.AlignmentFlag.AlignLeft)
+        layout.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.ExpandingFieldsGrow)
+
+        box.setLayout(layout)
+
+        parent_layout.addWidget(box)
+        return layout
+
+    @staticmethod
+    def create_input_field(initial_value="", background_color="#FCFCFC"):
+        field = QLineEdit(initial_value)
+        field.setStyleSheet(f"background-color: {background_color} ")
+        field.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        return field
+
+    @staticmethod
+    def create_form_input_row(
+        line_edit_value: str,
+        label_text: str,
+        parent_layout: QFormLayout,
+    ):
+        el = WidgetFactory.create_input_field(line_edit_value)
+        label = QLabel(label_text)
+        label.setStyleSheet("background-color: transparent;")
+        parent_layout.addRow(label, el)
+        return el
