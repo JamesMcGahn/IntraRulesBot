4ab4c6fcbc5c207b3e1d6d68b55a2fe6897fbbf3
James McGahn <jmcgahn@gmail.com>
2024-09-30 23:17:36 -0400

    update rules model

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .rules import RulesModel
+
+__all__ = ['RulesModel']
--- models/rules.py
+++ models/rules.py
@@ -1,13 +1,22 @@
-from PySide6.QtCore import QObject, Signal
+from PySide6.QtCore import QObject, Signal, Slot
+
+from utils.singletons import QSingleton
 
 
-class DataModel(QObject):
-    data_changed = Signal()
+class RulesModel(QObject, metaclass=QSingleton):
+    data_changed = Signal(list)
 
     def __init__(self):
         super().__init__()
+        print(f"RulesModel instance ID: {id(self)}")
         self.rules = []
 
+    @Slot(object)
     def add_rule(self, rule):
         self.rules.append(rule)
-        self.data_changed.emit()
+        self.data_changed.emit(self.rules)
+
+    @Slot(list)
+    def reset_model(self, rules):
+        self.rules = rules
+        self.data_changed.emit(self.rules)
