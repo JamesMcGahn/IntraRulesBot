3178ffcec3e5eb411d8a92a1dff7111f2f5ec5a1
James McGahn <jmcgahn@gmail.com>
2024-10-03 02:49:30 -0400

    add logic to retry errored rules when notified by rule worker

--- rulerunner/rule_runner_thread.py
+++ rulerunner/rule_runner_thread.py
@@ -1,7 +1,7 @@
 # from rule_runner_worker import RuleRunnerWorker
 import threading
+from collections import deque
 from concurrent.futures import ThreadPoolExecutor
-from queue import Queue
 
 from PySide6.QtCore import QMutex, QMutexLocker, QThread, QWaitCondition, Signal, Slot
 
@@ -27,14 +27,19 @@ class RuleRunnerThread(QThread):
         self.username = username
         self.password = password
         self.url = url
-        self.rules = Queue()
+        self.rules = deque()
         for rule in rules:
-            self.rules.put(rule)
+            self.rules.append(rule)
+
         self.driver_manager = WebDriverManager()
         self.driver = self.driver_manager.get_driver()
         self.wELI = WebElementInteractions(self.driver)
         self.executor = ThreadPoolExecutor(max_workers=8)
         self._rule_finished = False
+        self.current_rule = None
+        self.errors_in_a_row = 0
+        self.errored_rules = []
+        self.success_rules = []
 
     def run(self):
         self.receiver_thread_logs(
@@ -77,14 +82,17 @@ class RuleRunnerThread(QThread):
             self.close()
 
     def process_next_rule(self):
-        if not self.rules.empty():
+        print("lennnnnnnn", len(self.rules))
+        if len(self.rules) > 0:
+            print("erereere33333")
             try:
                 self.pause_if_needed(self._paused)
-                rule = self.rules.get()
-                rule_worker = RuleWorker(self.driver, rule)
-                rule_worker.finished.connect(self.on_rule_finished)
+                self.current_rule = self.rules.popleft()
+                rule_worker = RuleWorker(self.driver, self.current_rule)
                 rule_worker.send_logs.connect(self.receiver_thread_logs)
-                rule_worker.finished.connect(self.process_next_rule)
+                rule_worker.error.connect(self.on_rule_error)
+                rule_worker.error.connect(rule_worker.deleteLater)
+                rule_worker.finished.connect(self.on_rule_finished)
                 rule_worker.finished.connect(rule_worker.deleteLater)
                 self.executor.submit(rule_worker.do_work)
             except Exception as e:
@@ -93,16 +101,42 @@ class RuleRunnerThread(QThread):
                 )
 
         else:
+            print("shut down")
+            # TODO: Add logging for error rule flow
+            for rule in self.errored_rules:
+                print("errored", rule["rule_name"])
+            for rule in self.success_rules:
+                print("success", rule["rule_name"])
             self.executor.shutdown()
             self.process_finished_()
 
+    def on_rule_error(self):
+        self.errors_in_a_row += 1
+        print("received error from rule worker")
+        print("error_in_a_row", self.errors_in_a_row)
+        if self.errors_in_a_row < 2:
+            print("less than two")
+            print("trying next rule")
+            self.rules.appendleft(self.current_rule)
+            self.get_login_url()
+            self.start_login()
+
+        else:
+            print("appending")
+            self.errored_rules.append(self.current_rule)
+            print("next rule")
+            self.get_login_url()
+            self.start_login()
+
     def process_finished_(self):
-        self.finished.emit()
         self.driver_manager.close()
+        self.finished.emit()
 
     @Slot()
     def on_rule_finished(self):
         self.receiver_thread_logs("RuleWorker finished.", "INFO")
+        self.success_rules.append(self.current_rule)
+        self.errors_in_a_row = 0
         self.process_next_rule()
 
     def wait_for_rule_to_finish(self):
