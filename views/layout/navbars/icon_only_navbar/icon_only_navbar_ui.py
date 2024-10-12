f239159259f2204b935ee08f35858ba635d9100d
James McGahn <jmcgahn@gmail.com>
2024-10-12 19:26:30 -0400

    refactor Icon Only and Icon Text Navbars

--- views/layout/navbars/icon_only_navbar/icon_only_navbar_ui.py
+++ views/layout/navbars/icon_only_navbar/icon_only_navbar_ui.py
@@ -1,5 +1,4 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (
     QPushButton,
     QSizePolicy,
@@ -8,14 +7,19 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
+from components.helpers import WidgetFactory
+
 
 class IconOnlyNavBarView(QWidget):
+    """Compressed Side Navigation Bar - Icon Only Buttons - UI Components"""
+
     def __init__(self):
         super().__init__()
         self.init_ui()
         self.setObjectName("icon_only_widget_ui")
 
     def init_ui(self):
+        """Add UI Components"""
         self.setMaximumSize(QSize(70, 16777215))
         self.setMinimumSize(QSize(70, 16777215))
         self.setAttribute(Qt.WA_StyledBackground, True)
@@ -23,50 +27,46 @@ class IconOnlyNavBarView(QWidget):
         self.icon_nav_vlayout.setObjectName("icon_nav_vlayout")
         self.icon_btn_layout = QVBoxLayout()
         self.icon_btn_layout.setObjectName("icon_btn_layout_ico")
+
         self.keys_btn_ico = QPushButton()
         self.keys_btn_ico.setObjectName("keys_btn_ico")
-
-        icon = QIcon()
-        icon.addFile(
-            ":/images/key_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon.addFile(":/images/key_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On)
-        self.keys_btn_ico.setIcon(icon)
-        self.keys_btn_ico.setIconSize(QSize(100, 20))
-        self.keys_btn_ico.setCheckable(True)
-        self.keys_btn_ico.setAutoExclusive(True)
-
         self.icon_btn_layout.addWidget(self.keys_btn_ico)
 
         self.rules_btn_ico = QPushButton()
         self.rules_btn_ico.setObjectName("rules_btn_ico")
-        icon1 = QIcon()
-        icon1.addFile(
-            ":/images/edit_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon1.addFile(
-            ":/images/edit_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On
-        )
-        self.rules_btn_ico.setIcon(icon1)
-        self.rules_btn_ico.setIconSize(QSize(100, 20))
-        self.rules_btn_ico.setCheckable(True)
-        self.rules_btn_ico.setAutoExclusive(True)
-
         self.icon_btn_layout.addWidget(self.rules_btn_ico)
 
         self.logs_btn_ico = QPushButton()
         self.logs_btn_ico.setObjectName("logs_btn_ico")
-        icon2 = QIcon()
-        icon2.addFile(
-            ":/images/log_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon2.addFile(":/images/log_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On)
-        self.logs_btn_ico.setIcon(icon2)
-        self.logs_btn_ico.setIconSize(QSize(100, 20))
-        self.logs_btn_ico.setCheckable(True)
-        self.logs_btn_ico.setAutoExclusive(True)
-
         self.icon_btn_layout.addWidget(self.logs_btn_ico)
+
+        self.settings_btn_ico = QPushButton()
+        self.settings_btn_ico.setObjectName("settings_btn_ico")
+
+        self.signout_btn_ico = QPushButton()
+        self.signout_btn_ico.setObjectName("signout_btn_ico")
+
+        icons = [
+            (self.keys_btn_ico, ":/images/key_off.png", ":/images/key_on.png"),
+            (self.rules_btn_ico, ":/images/edit_off.png", ":/images/edit_on.png"),
+            (self.logs_btn_ico, ":/images/log_off.png", ":/images/log_on.png"),
+            (
+                self.settings_btn_ico,
+                ":/images/settings_off.png",
+                ":/images/settings_on.png",
+            ),
+            (
+                self.signout_btn_ico,
+                ":/images/signout_off.png",
+                ":/images/signout_on.png",
+            ),
+        ]
+
+        for icon in icons:
+            parent, image_loc_1, image_loc_2 = icon
+            WidgetFactory.create_icon(
+                parent, image_loc_1, 100, 20, True, image_loc_2, True
+            )
 
         self.icon_nav_vlayout.addLayout(self.icon_btn_layout)
 
@@ -75,34 +75,6 @@ class IconOnlyNavBarView(QWidget):
         )
 
         self.icon_nav_vlayout.addItem(self.verticalSpacer_2)
-
-        self.settings_btn_ico = QPushButton()
-        self.settings_btn_ico.setObjectName("settings_btn_ico")
-        icon4 = QIcon()
-        icon4.addFile(
-            ":/images/settings_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon4.addFile(
-            ":/images/settings_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On
-        )
-        self.settings_btn_ico.setIcon(icon4)
-        self.settings_btn_ico.setIconSize(QSize(100, 20))
-        self.settings_btn_ico.setCheckable(True)
-        self.settings_btn_ico.setAutoExclusive(True)
-
-        self.signout_btn_ico = QPushButton()
-        self.signout_btn_ico.setObjectName("signout_btn_ico")
-        icon5 = QIcon()
-        icon5.addFile(
-            ":/images/signout_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon5.addFile(
-            ":/images/signout_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On
-        )
-        self.signout_btn_ico.setIcon(icon5)
-        self.signout_btn_ico.setIconSize(QSize(100, 20))
-        self.signout_btn_ico.setCheckable(True)
-        self.signout_btn_ico.setAutoExclusive(True)
 
         self.icon_nav_vlayout.addWidget(self.settings_btn_ico)
         self.icon_nav_vlayout.addWidget(self.signout_btn_ico)
--- views/layout/navbars/icon_text_navbar/icon_text_navbar_ui.py
+++ views/layout/navbars/icon_text_navbar/icon_text_navbar_ui.py
@@ -1,5 +1,4 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (
     QPushButton,
     QSizePolicy,
@@ -8,130 +7,77 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
-from components.helpers import StyleHelper
+from components.helpers import StyleHelper, WidgetFactory
 
 
 class IconTextNavBarView(QWidget):
+    """Expanded Side Navigation Bar - Icons and Text Buttons - UI Components"""
+
     def __init__(self):
         super().__init__()
         self.init_ui()
         self.setObjectName("icon_text_widget_ui")
 
     def init_ui(self):
+        """Add UI Components"""
         self.setMaximumSize(QSize(250, 16777215))
 
         StyleHelper.drop_shadow(self)
 
         self.setAttribute(Qt.WA_StyledBackground, True)
-        icon = QIcon()
-        icon.addFile(
-            ":/images/key_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon.addFile(":/images/key_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On)
 
-        icon1 = QIcon()
-        icon1.addFile(
-            ":/images/edit_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon1.addFile(
-            ":/images/edit_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On
-        )
-        icon2 = QIcon()
-        icon2.addFile(
-            ":/images/log_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon2.addFile(":/images/log_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On)
-        icon4 = QIcon()
-        icon4.addFile(
-            ":/images/settings_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon4.addFile(
-            ":/images/settings_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On
-        )
-        icon5 = QIcon()
-        icon5.addFile(
-            ":/images/signout_off.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off
-        )
-        icon5.addFile(
-            ":/images/signout_on.png", QSize(), QIcon.Mode.Normal, QIcon.State.On
-        )
         self.icon_text_nav_vlayout = QVBoxLayout(self)
         self.icon_text_nav_vlayout.setObjectName("icon_text_nav_vlayout")
+
         self.icon_btn_layout = QVBoxLayout()
         self.icon_btn_layout.setObjectName("icon_btn_layout_ict")
-        self.keys_btn_ict = QPushButton(self)
-        self.keys_btn_ict.setObjectName("keys_btn_ict")
-        self.keys_btn_ict.setIcon(icon)
-        self.keys_btn_ict.setIconSize(QSize(100, 20))
-        self.keys_btn_ict.setCheckable(True)
-        self.keys_btn_ict.setAutoExclusive(True)
 
+        self.keys_btn_ict = QPushButton(" Login Creds")
+        self.keys_btn_ict.setObjectName("keys_btn_ict")
         self.icon_btn_layout.addWidget(self.keys_btn_ict)
 
-        self.rules_btn_ict = QPushButton(self)
+        self.rules_btn_ict = QPushButton(" Rules")
         self.rules_btn_ict.setObjectName("rules_btn_ict")
-        sizePolicy1 = QSizePolicy(
-            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
-        )
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(
-            self.rules_btn_ict.sizePolicy().hasHeightForWidth()
-        )
-        self.rules_btn_ict.setSizePolicy(sizePolicy1)
-        self.rules_btn_ict.setMaximumSize(QSize(16777215, 16777215))
-        self.rules_btn_ict.setIcon(icon1)
-        self.rules_btn_ict.setIconSize(QSize(100, 20))
-        self.rules_btn_ict.setCheckable(True)
-        self.rules_btn_ict.setAutoExclusive(True)
-
         self.icon_btn_layout.addWidget(self.rules_btn_ict)
 
-        self.logs_btn_ict = QPushButton(self)
+        self.logs_btn_ict = QPushButton(" Logs")
         self.logs_btn_ict.setObjectName("logs_btn_ict")
-        sizePolicy2 = QSizePolicy(
-            QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed
-        )
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(
-            self.logs_btn_ict.sizePolicy().hasHeightForWidth()
-        )
-        self.logs_btn_ict.setSizePolicy(sizePolicy2)
-        self.logs_btn_ict.setIcon(icon2)
-        self.logs_btn_ict.setIconSize(QSize(100, 20))
-        self.logs_btn_ict.setCheckable(True)
-        self.logs_btn_ict.setAutoExclusive(True)
-
         self.icon_btn_layout.addWidget(self.logs_btn_ict)
 
         self.icon_text_nav_vlayout.addLayout(self.icon_btn_layout)
-
         self.verticalSpacer_3 = QSpacerItem(
             20, 589, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding
         )
 
         self.icon_text_nav_vlayout.addItem(self.verticalSpacer_3)
 
-        self.settings_btn_ict = QPushButton(self)
+        self.settings_btn_ict = QPushButton("Settings")
         self.settings_btn_ict.setObjectName("settings_btn_ict")
-        self.settings_btn_ict.setIcon(icon4)
-        self.settings_btn_ict.setIconSize(QSize(100, 20))
-        self.settings_btn_ict.setCheckable(True)
-        self.settings_btn_ict.setAutoExclusive(True)
-
-        self.signout_btn_ict = QPushButton(self)
-        self.signout_btn_ict.setObjectName("signout_btn_ict")
-        self.signout_btn_ict.setIcon(icon5)
-        self.signout_btn_ict.setIconSize(QSize(100, 20))
-        self.signout_btn_ict.setCheckable(True)
-        self.signout_btn_ict.setAutoExclusive(True)
-
-        self.keys_btn_ict.setText(" Login Creds")
-        self.rules_btn_ict.setText(" Rules")
-        self.logs_btn_ict.setText(" Logs")
-        self.settings_btn_ict.setText("Settings")
-        self.signout_btn_ict.setText("Exit")
-
         self.icon_text_nav_vlayout.addWidget(self.settings_btn_ict)
+
+        self.signout_btn_ict = QPushButton("Exit")
+        self.signout_btn_ict.setObjectName("signout_btn_ict")
         self.icon_text_nav_vlayout.addWidget(self.signout_btn_ict)
+
+        icons = [
+            (self.keys_btn_ict, ":/images/key_off.png", ":/images/key_on.png"),
+            (self.rules_btn_ict, ":/images/edit_off.png", ":/images/edit_on.png"),
+            (self.logs_btn_ict, ":/images/log_off.png", ":/images/log_on.png"),
+            (
+                self.settings_btn_ict,
+                ":/images/settings_off.png",
+                ":/images/settings_on.png",
+            ),
+            (
+                self.signout_btn_ict,
+                ":/images/signout_off.png",
+                ":/images/signout_on.png",
+            ),
+        ]
+
+        for icon in icons:
+            parent, image_loc_1, image_loc_2 = icon
+            WidgetFactory.create_icon(
+                parent, image_loc_1, 100, 20, True, image_loc_2, True
+            )
+            StyleHelper.drop_shadow(parent)
