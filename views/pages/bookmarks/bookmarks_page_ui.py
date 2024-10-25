3e2e97b45f6d96d99480f0375b2cb9e8ac2dff44
James McGahn <jmcgahn@gmail.com>
2024-10-25 02:18:03 -0400

    navigation methods, navigation btns

--- views/pages/bookmarks/bookmarks_page_ui.py
+++ views/pages/bookmarks/bookmarks_page_ui.py
@@ -11,6 +11,7 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
+from components.buttons import EditorActionButton
 from components.helpers import WidgetFactory
 
 
@@ -69,13 +70,39 @@ class BookMarksPageView(QWidget):
 
         rule_set_layout = QVBoxLayout(rule_set_widget)
         h_layout = QHBoxLayout()
+
+        self.arrow_up = EditorActionButton("")
+        self.arrow_down = EditorActionButton("")
+        WidgetFactory.create_icon(
+            self.arrow_up,
+            ":/images/up_arrow_off_b.png",
+            25,
+            20,
+            True,
+            ":/images/up_arrow_on.png",
+            False,
+        )
+        WidgetFactory.create_icon(
+            self.arrow_down,
+            ":/images/down_arrow_off_b.png",
+            25,
+            20,
+            True,
+            ":/images/down_arrow_on.png",
+            False,
+        )
+        # Nav Buttons
+        nav_box = QVBoxLayout()
+        nav_box.addWidget(self.arrow_up)
+        nav_box.addWidget(self.arrow_down)
+
+        h_layout.addLayout(nav_box)
         rule_set_layout.addLayout(h_layout)
 
-        self.delete_button = QPushButton("Delete")
-        h_layout.addWidget(self.delete_button)
-
+        # List Widget
         self.list_widget = QListWidget()
 
+        # Rule Set Details
         rule_set_details = QVBoxLayout()
         self.rule_set_name = QLineEdit()
         self.rule_set_description = QTextEdit()
@@ -85,16 +112,44 @@ class BookMarksPageView(QWidget):
         rule_set_layout.addLayout(rule_set_details)
 
         h_layout.addWidget(self.list_widget)
+
+        # Action Buttons Bar
+        action_btns_layout = QHBoxLayout()
+        self.delete_button = QPushButton("Delete")
+        action_btns_layout.addWidget(self.delete_button)
+
+        rule_set_layout.addLayout(action_btns_layout)
+
         inner_layout.addRow(rule_set_widget)
+
         self.delete_button.clicked.connect(self.remove_item)
         self.list_widget.itemSelectionChanged.connect(self.on_selection_changed)
+        self.arrow_up.clicked.connect(self.navigate_up)
+        self.arrow_down.clicked.connect(self.navigate_down)
+
+    def navigate_up(self):
+        if self.list_widget.currentRow() > 0:
+            self.list_widget.setCurrentRow(self.list_widget.currentRow() - 1)
+        else:
+            self.list_widget.setCurrentRow(self.list_widget.count() - 1)
+
+    def navigate_down(self):
+        print(self.list_widget.currentRow(), self.list_widget.count())
+        if self.list_widget.currentRow() != self.list_widget.count() - 1:
+            self.list_widget.setCurrentRow(self.list_widget.currentRow() + 1)
+        else:
+            self.list_widget.setCurrentRow(0)
 
     def init_rule_set(self, rule_sets: list) -> None:
 
         for rule_set in rule_sets:
             self.add_rule_set(rule_set)
 
+        if self.list_widget.count() > 0:
+            self.list_widget.setCurrentRow(0)
+
     def on_selection_changed(self):
+        print(self.list_widget.currentRow(), self.list_widget.count())
         list_item = self.list_widget.currentItem()
         list_item_id = list_item.data(Qt.UserRole)
         index = self.list_widget.currentRow()
@@ -110,7 +165,6 @@ class BookMarksPageView(QWidget):
 
     @Slot(object)
     def add_rule_set(self, rule_set: object) -> None:
-        print("sss", rule_set)
         list_item = QListWidgetItem(rule_set["name"])
         list_item.setData(Qt.UserRole, rule_set["id"])
         self.list_widget.addItem(list_item)
